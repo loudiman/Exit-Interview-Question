4854c3f3dabe915e6f50dab5eeccb7bce65b2b61
Lou <2233672@slu.edu.ph>
2024-11-15 23:49:02 +0800

    Updated the routing of uri

--- server/php/routes.php
+++ server/php/routes.php
@@ -3,8 +3,11 @@
 $router->get('/', 'session/create.php');
 $router->post('/', 'session/store.php');
 
+$router->get('/student', 'student/index.php');
+
 $router->get('/student/surveys', 'surveys/index.php');
 $router->get('/student/survey', 'surveys/show.php');
-$router->post('/student/survey', 'surveys/store.php');
+// $router->post('/student/survey', 'surveys/store.php');
 
-$router->get('/student/survey/questionnaire', 'temp/index.php');
+$router->get('/student/survey/questionnaires', 'questionnaires/index.php');
+$router->post('/student/survey/questionnaires', 'questionnaires/store.php');
