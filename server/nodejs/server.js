b51c145ff0b448eecb4300713048b32c99293fc7
Mark Lestat Agustin <85033864+VladTemp27@users.noreply.github.com>
2024-11-16 03:25:40 +0000

    + testing running a express application from another script

--- server/nodejs/server.js
+++ server/nodejs/server.js
@@ -1,17 +1,6 @@
-const express = require('express');
-const cors = require('cors')
-const app = express();
-
-app.use(cors())
+const mainRuntime = require("./runtimes/main")
 
 
-// Home route
-app.get('/', (req, res) => {
-  res.send('Welcome to the home page!');
-});
-
-
-const PORT = 80
-app.listen(PORT, ()=>{
-  console.log('Server is running');
-});
+mainRuntime.listen(80, () => {
+  console.log("server running")
+})
