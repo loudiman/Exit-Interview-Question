2997441f965c463ad002c3e423a823af47fb53e2
Lou <2233672@slu.edu.ph>
2024-11-19 00:42:19 +0800

    Removed unnecessary json manipulation

--- server/php/app/Http/controllers/student/index.php
+++ server/php/app/Http/controllers/student/index.php
@@ -6,19 +6,13 @@ use Core\Database;
 try {
     $surveys = App::resolve(Database::class)->query(
         'SELECT z.survey_id, x.survey_title, z.responded, x.period_start, x.period_end FROM survey x INNER JOIN student y ON x.program_id = y.program_id INNER JOIN (SELECT survey_id, responded FROM responders) z ON z.survey_id = x.survey_id WHERE y.username = ? and x.status = "published"', [2233672] // To be replaced with [$_SESSION['user']['username']] when logout is implemented.
-    )->get();
-    
-    $response = array();
-    for ($i = 0; $i < count($surveys); $i++) {
-        $keys = ['survey_id', 'survey_title', 'responded', 'period-start', 'period-end'];
-        $values = [];
-        for ($j = 0; $j < count($surveys[$i]) && count($keys); $j++) {
-            array_push($values, $surveys[$i][$j]);
-        }
-        array_push($response, array_combine($keys, $values));
-    }
-    // var_dump(json_encode(['surveys' => $response]));
-    echo json_encode(['surveys' => $response]);
+    )->assoc_get();
+
+    // print_r($surveys);
+    // print_r(count($surveys));
+    // print_r($surveys[0]);
+    // print_r(json_encode(['surveys' => $surveys]));
+    echo json_encode(['surveys' => $surveys]);
 
 } catch (Exception $e) {
     http_response_code(500);
