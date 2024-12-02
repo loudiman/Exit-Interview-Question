66ebffae1c83cddf7b79e8a19355c334bd55a207
Lestat <2233915@slu.edu.ph>
2024-12-02 22:03:59 +0800

    + middleware for clientside js and css

--- server/nodejs/main-runtime.js
+++ server/nodejs/main-runtime.js
@@ -5,10 +5,10 @@ const path = require('path')
 
 const publicDir = path.join(__dirname, "resources")
 
-app.use('static/css', express.static(path.join(__dirname,"resources","css")))
+//Middleware that exposes the css resources statically
+app.use('/static/css', express.static(path.join(__dirname,"resources","css")))
 
-app.get('/admin/profile',(req, res)=>{
-    res.sendFile(path.join(publicDir,"views","admin-profile.html"))
-})
+//Middleware that exposes the js resources statically
+app.use('/static/js',express.static(__dirname, "resources", "js"))
 
 module.exports = app
