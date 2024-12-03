2b9953c98e311a863696eb44ba0889a979a0e188
Marven <luis.marvenjoffre@gmail.com>
2024-12-04 00:02:15 +0800

    + edited some things

--- server/nodejs/main-runtime.js
+++ server/nodejs/main-runtime.js
@@ -9,6 +9,13 @@ const publicDir = path.join(__dirname, "resources")
 app.use('/static/css', express.static(path.join(__dirname,"resources","css")))
 
 //Middleware that exposes the js resources statically
-app.use('/static/js',express.static(__dirname, "resources", "js"))
+app.use('/static/js',express.static(path.join(__dirname,"resources","js")))
+
+app.use(`/static/images`,express.static(path.join(__dirname,"resources","images")))
+
+app.get("/",(req, res)=>{
+   const adminCreation = path.join(__dirname,"resources","views","survey_creation.html");
+   res.sendFile(adminCreation);
+});
 
 module.exports = app
