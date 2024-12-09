b586f2b031350737df053a3c03b8e3fabeafdb0c
Lou <2233672@slu.edu.ph>
2024-12-10 01:43:54 +0800

    Updated routes for viewing and updating student surveys

--- server/php/routes.php
+++ server/php/routes.php
@@ -7,7 +7,9 @@ $router->delete('/session/logout', 'session/destroy.php')->only('auth');
 $router->get('/student', 'student/index.php')->only('auth');
 
 $router->get('/student/surveys', 'surveys/index.php')->only('auth');
-$router->get('/student/survey', 'surveys/show.php')->only('auth');
+
+$router->get('/student/survey/viewsurvey', 'surveys/show.php')->only('auth');
+$router->get('/student/survey/closedsurvey', 'surveys/update.php')->only('auth');
 // $router->post('/student/survey', 'surveys/store.php');
 
 $router->get('/student/survey/questionnaires', 'questionnaires/index.php')->only('auth');
