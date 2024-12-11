b0207bac912f6b51a5fdb41567f14b0d04a4cb8b
Lou <2233672@slu.edu.ph>
2024-12-11 10:15:36 +0800

    Major Update

--- server/node/routes.js
+++ server/node/routes.js
@@ -6,32 +6,37 @@ const path = require('path');
 router.use(express.static(path.join(__dirname, 'public')));
 
 router.get('/admin/dashboard', (req, res) => {
-    // res.json({ message: 'Test route working' });
     res.sendFile(path.join(__dirname, 'public', 'resources', 'views', 'admin-dashboard.html'));
 });
 
 router.get('/admin/surveys', (req, res) => {
-    // res.json({ message: 'Test route working' });
     res.sendFile(path.join(__dirname, 'public', 'resources', 'views', 'admin-survey-templates.html'));
 });
 
-router.get('/admin/surveys/edit', (req, res) => {
-    // res.json({ message: 'Test route working' });
+// force to use /admin/surveys/creation by figma design instead of straight redirection to /admin/surveys/edit 
+router.get('/admin/surveys/creation', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'resources', 'views', 'admin-survey-creation.html'));
 });
 
 router.get('/admin/surveys/publish', (req, res) => {
-    // res.json({ message: 'Test route working' });
     res.sendFile(path.join(__dirname, 'public', 'resources', 'views', 'admin-survey-publish.html'));
 });
 
+// just admit that /admin/surveys/creation is not needed
+router.get('/admin/surveys/view', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'resources', 'views', 'admin-survey-view.html'));
+});
+
+// just fucking admit its bad
+router.get('/admin/surveys/edit', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'resources', 'views', 'admin-survey-edit.html'));
+});
+
 router.get('/admin/profile', (req, res) => {
-    // res.json({ message: 'Test route working' });
     res.sendFile(path.join(__dirname, 'public', 'resources', 'views', 'admin-profile.html'));
 });
 
 router.get('/admin/register', (req, res) => {
-    // res.json({ message: 'Test route working' });
     res.sendFile(path.join(__dirname, 'public', 'resources', 'views', 'index.html'));
 });
 
