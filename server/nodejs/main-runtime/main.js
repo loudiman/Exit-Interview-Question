d12c4cf1961cb5082ddbd14655b74524df97b071
Gebreyl <2233944@slu.edu.ph>
2024-12-11 23:21:42 +0800

    + Added missing necessary static resources and cleanup

--- server/nodejs/main-runtime/main.js
+++ server/nodejs/main-runtime/main.js
@@ -3,23 +3,19 @@ const main = express()
 const adminRoutes= require('./services/resource-service/routes/admin-routes')
 
 const path = require('path')
+const publicDir = (path.join(__dirname, "../resources"))
 
-const publicDir = path.join(__dirname, "../resources")
+//Middleware that exposes resources statically
+main.use('/static/css', express.static(path.join(publicDir, 'css')));
+main.use('/static/fonts', express.static(path.join(publicDir, 'fonts')));
+main.use('/static/images', express.static(path.join(publicDir, 'images')));
+main.use('/static/js', express.static(path.join(publicDir, 'js')));
 
-//Middleware that exposes the css resources statically
-main.use('/static/css', express.static(path.join(publicDir,"css")))
-
-//Middleware that exposes the js resources statically
-main.use('/static/js',express.static(path.join(publicDir,"js")))
-
-main.use(`/static/images`,express.static(path.join(publicDir,"images")))
-
-//user the admin routes
+//Use the admin routes
 main.use(adminRoutes)
-main.use(creationRoutes)
 
-main.get('/', (req, res) => {
-    const htmlPath = path.join(publicDir, 'views', 'index.html')
+main.get('/admin', (req, res) => {
+    const htmlPath = path.join(publicDir, "index.html")
     res.sendFile(htmlPath)
 })
 
