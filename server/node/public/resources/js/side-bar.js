386f4c697d13eea32856d4bb9f22a561f4e8e819
Lou <2233672@slu.edu.ph>
2024-12-14 23:09:09 +0800

    Major update of the project

--- server/node/public/resources/js/side-bar.js
+++
@@ -1,9 +0,0 @@
-// Sidebar toggle
-document.getElementById('sidebarToggle').addEventListener('click', toggleSidebar);
-
-function toggleSidebar() {
-    const sidebar = document.getElementById('sidebar');
-    const mainContent = document.getElementById('mainContent');
-    sidebar.classList.toggle('hidden');
-    mainContent.classList.toggle('full-width');
-}
